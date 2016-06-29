bdae9911235c1db32cb6705d752be9fe8fc6cb56
chenfenglong <chenfenglong@koudai.com>
2016-06-29 19:03:42 +0800

    添加详情js

--- HelloWorld/SearchResults.js
+++ HelloWorld/SearchResults.js
@@ -7,8 +7,39 @@ import {
   View,
   TouchableHighlight,
   ListView,
-  Text
+  Text,
+  NavigatorIOS
  } from 'react-native';
+ 
+ var PropertyView = require('./PropertyView');
+ 
+ var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 class SearchResults extends Component{
     constructor(props){
@@ -20,12 +51,31 @@ class SearchResults extends Component{
         };
     }
     
+    rowPressed(propertyGuid){
+        var property = this.props.listings.filter(prop => prop.guid === propertyGuid)[0];
+        
+        this.props.navigator.push({
+        title: "Property",
+        component: PropertyView,
+        passProps: {property: property}
+  });
+    }
+    
     renderRow(rowData, sectionID, rowID){
         return (
-          <TouchableHighlight
+          <TouchableHighlight onPress={() => this.rowPressed(rowData.guid)}
             underlayColor='#dddddd'>
             <View>
-                <Text>{rowData.title}</Text>
+                <View style={styles.rowContainer}>
+                    <Image style={styles.thumb} source={{uri: rowData.img_url}} />
+                    <View style={styles.textContainer}>
+                        <Text style={styles.price}>£{rowData.price}</Text>
+                        <Text style={styles.title}
+                                numberOfLines={1}>{rowData.title}</Text>
+                    </View>
+                </View>
+                
+                <View style={styles.separator}/>
             </View>  
           </TouchableHighlight>
         );
